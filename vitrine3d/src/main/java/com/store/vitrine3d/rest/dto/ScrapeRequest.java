3b4b1e52e963461dfb64e91a6f38dd9d9d6ac865
Henrique-Rmc <henriqueribcruz_@hotmail.com>
2026-06-22 18:02:19 -0300

    security fixes

--- vitrine3d/src/main/java/com/store/vitrine3d/rest/dto/ScrapeRequest.java
+++ vitrine3d/src/main/java/com/store/vitrine3d/rest/dto/ScrapeRequest.java
@@ -1,10 +1,16 @@
 package com.store.vitrine3d.rest.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class ScrapeRequest {
-    @NotBlank(message = "URL is required")
+
+    @NotBlank(message = "A URL é obrigatória")
+    @Pattern(
+        regexp = "^https://(?:www\\.)?makerworld\\.com/.+",
+        message = "Apenas URLs do MakerWorld são aceitas (https://makerworld.com/...)"
+    )
     private String url;
 }
